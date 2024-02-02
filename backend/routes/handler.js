3783f4712230bb936c7e02262c83b16e9f2a1b96
Ben <91610521+bendaacz@users.noreply.github.com>
2024-02-02 09:25:19 +0000

    backend 1

---
+++ backend/routes/handler.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/log-in", (req, res) => {
+    const str = [{
+        "name": "log in.",
+        "msg": "log in!",
+        "username": "log in?"
+    }];
+    res.end(JSON.stringify(str));
+});
+
+router.post("/sign-in", (req, res) => {
+    res.end("NA");
+});
+
+module.exports = router;

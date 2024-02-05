149eba8d16f42981dd3ee75ab1de41e1bc28b4f8
Bendaa <benedikt.student@gmail.com>
2024-02-05 17:58:34 +0100

    fix: 404 api error

--- BACKEND/routes/handler.js
+++ BACKEND/routes/handler.js
@@ -1,21 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/*', (req, res) => {
-    res.send('400 Bad Request');
-  });
-
 router.get("/log-in", (req, res) => {
-    const str = [{
-        "name": "log in.",
-        "msg": "log in!",
-        "username": "log in?"
-    }];
-    res.end(JSON.stringify(str));   
+  const str = [
+    {
+      name: "log in.",
+      msg: "log in!",
+      username: "log in?",
+    },
+  ];
+  res.end(JSON.stringify(str));
 });
 
 router.post("/sign-in", (req, res) => {
-    res.end("NA");
+  res.end("NA");
 });
 
 module.exports = router;

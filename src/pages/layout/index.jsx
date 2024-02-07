34f803e8e860c8846bbf2bf849687758a14214d8
Bendaa <benedikt.student@gmail.com>
2024-02-07 15:07:28 +0100

    fix: icons look better now

--- src/pages/layout/index.jsx
+++ src/pages/layout/index.jsx
@@ -1,14 +1,13 @@
 import MediaQuery from 'react-responsive';
 import Nav from "../navbar/index";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBoltLightning, faCoffee, faDollarSign, faServer } from '@fortawesome/free-solid-svg-icons'
+import { faBoltLightning, faDollarSign, faServer } from '@fortawesome/free-solid-svg-icons'
 
 function Layout() {
     return (
-      <>
 <MediaQuery minWidth={1060}>
 <Nav />
-<h1 className="text-white font-header text-xl flex mt-[50px] justify-center h-[228px]">infinity</h1>
+<h1 className=" text-white font-header text-xl flex mt-[50px] justify-center h-[228px]">infinity</h1>
 <div className="flex flex-col items-center justify-center">
 <p className="font-header flex text-[#999999] mt-[10px] ">SECURED WITH CONFIDENCE!</p>
 <p className="font-header flex text-[#CFCFCF] ml-[18px]">PEACEFUL NIGHTS START NOW.</p>
@@ -23,15 +22,15 @@ function Layout() {
         <div className="flex justify-between items-center mt-[40px] whitespace-nowrap">
           <div className="flex gap-[200px] bg-[#262626]">
             <div className="flex flex-col items-center text-white bg-[#262626]">
-              <FontAwesomeIcon size='10x' className='bg-[#262626]' icon={faBoltLightning} />
+              <FontAwesomeIcon size='10x' className='bg-[#262626] text-[#7cc4ff]' icon={faBoltLightning} />
               <p className='bg-[#262626] mt-[50px] text-[25px] font-normal'>{'>'}5ms latency</p>
             </div>
             <div className="flex flex-col items-center text-white bg-[#262626]">
-              <FontAwesomeIcon size='10x' className='bg-[#262626]' icon={faDollarSign} />
+              <FontAwesomeIcon size='10x' className='bg-[#262626] text-[#9da5ff]' icon={faDollarSign} />
               <p className='bg-[#262626] mt-[50px] text-[25px] font-normal'>$100 credit bonus</p>
             </div>
             <div className="flex flex-col items-center text-white bg-[#262626]">
-              <FontAwesomeIcon className='bg-[#262626]' size='10x' icon={faServer} />
+              <FontAwesomeIcon className='bg-[#262626] text-[#bf87ff]' size='10x' icon={faServer} />
               <p className='bg-[#262626] mt-[50px] text-[25px] font-normal'>99.98% uptime</p>
             </div>
           </div>
@@ -39,7 +38,6 @@ function Layout() {
       </div>
     </div>
   </MediaQuery>
-  </>
     );
 }
 

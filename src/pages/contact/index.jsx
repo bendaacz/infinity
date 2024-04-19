6c72dc84948eceecce844d951d2d76db4eb09cce
Ben <91610521+bendaacz@users.noreply.github.com>
2024-04-19 16:22:27 +0000

    feat: contact page completed

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -19,13 +19,29 @@ function Contact() {
   }, [loading]);
     return (
         <>
-          <MediaQuery minWidth={1060}>
+      <MediaQuery minWidth={1360}>
             <Nav />
         {loading ? (
           <Loader type="ball-grid-pulse" />
         ) : (
           <div className={`h-[1500px] ${fadeIn ? 'fade-in' : ''}`}>
-                <p className="text-base text-white">contact</p>
+            <p className="mt-[150px] font-light text-white text-[24px] flex justify-center">contact me on</p>
+            <div className='font-normal text-white text-[72px] flex justify-center justify-items center'>
+              <div className='hover:underline'>Instagram</div>&nbsp;/&nbsp;<div className='hover:underline'>Email</div>&nbsp;/&nbsp;<div className='hover:underline'>WhatsApp</div>
+            </div>
+          </div>
+        )}
+      </MediaQuery>
+      <MediaQuery minWidth={1060} maxWidth={1360}>
+        {loading ? (
+          <Loader type="ball-grid-pulse" />
+        ) : (
+          <div className={`h-[1500px] ${fadeIn ? 'fade-in' : ''}`}>
+            <Nav />
+            <p className="mt-[150px] font-light text-white text-[24px] flex justify-center">contact me on</p>
+            <p className='font-normal text-white text-[72px] flex justify-center justify-items center hover:underline'>Instagram</p>
+            <p className='font-normal text-white text-[72px] flex justify-center justify-items center hover:underline'>Email</p>
+            <p className='font-normal text-white text-[72px] flex justify-center justify-items center hover:underline'>WhatsApp</p>
               </div>
             )}
           </MediaQuery>

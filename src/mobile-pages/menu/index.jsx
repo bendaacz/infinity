fe3f67604a185d467a139fe83ab6666481258905
Ben <91610521+bendaacz@users.noreply.github.com>
2024-04-21 11:07:44 +0000

    feat: mobile menu is fully functional

--- src/mobile-pages/menu/index.jsx
+++ src/mobile-pages/menu/index.jsx
@@ -5,18 +5,20 @@ import { faX } from '@fortawesome/free-solid-svg-icons';
 export default function Menu() {
     return (
         <>
-            <div className="flex justify-end mt-[10px] mr-[10px]">
-                <Link to="/">
-                    <FontAwesomeIcon className='text-white' size='3x' icon={faX} />
-                </Link>
-            </div>
-            <div className="border flex justify-center items-center fixed">
-                <p className="font-normal text-white">home</p>
-                <p className="font-light text-white">contact</p>
-                <p className="font-light text-white">about</p>
+            <div className="">
+                <div className="h-[50vh] flex-row items-center space-y-[1vh]">
+                    <div className="flex justify-end mt-[15px] mr-[20px]">
+                        <Link to="/">
+                            <FontAwesomeIcon className='text-white' size='3x' icon={faX} />
+                        </Link>
+                    </div>
+                    <p className="font-normal text-white text-[50px] flex justify-center">home</p>
+                    <p className="font-light text-[#7cc4ff] text-[40px] flex justify-center">contact</p>
+                    <p className="font-light text-white text-[40px] flex justify-center">about</p>
+                    </div>
+                    <p className="font-light text-white text-[45px] flex justify-center">sign up</p>
+                    <p className="font-light text-[#bf87ff] text-[45px] flex justify-center">sign in</p>
 
-                <p className="font-light text-white">sign up</p>
-                <p className="font-light text-white">sign in</p>
             </div>
         </>
     );

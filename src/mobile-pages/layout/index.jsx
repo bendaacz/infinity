9da2fcf637f4f4ae3af74a0766b57e6c49ec337c
Ben <91610521+bendaacz@users.noreply.github.com>
2024-04-21 12:18:01 +0000

    feat: heading and description added

--- src/mobile-pages/layout/index.jsx
+++ src/mobile-pages/layout/index.jsx
@@ -1,11 +1,19 @@
-import MobileNav from "../../mobile-pages/navbar/index"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { Link } from "react-router-dom";
+import MobileNav from "../navbar/index"
 
-function Layout () {
+export default function Layout() {
     return (
-        <div className="h-[700px]">
-        <MobileNav />
-        </div>
+        <>
+            <MobileNav />
+            <div>
+                <h1 className='flex justify-center text-white font-header text-[75px] mt-[20px]'>infinity</h1>
+                <div className="flex flex-col items-center justify-center">
+                    <p className="font-header flex text-[#999999] text-[12px] mt-[0px] mr-[5vh]">SECURED WITH CONFIDENCE!</p>
+                    <p className="font-header flex text-[#CFCFCF] text-[12px] mr-[3vh]">PEACEFUL NIGHTS START NOW.</p>
+                </div>
+            </div>
+        </>
     );
 }
-
-export default Layout;

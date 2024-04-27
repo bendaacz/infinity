ed3060bd64b9be95397b7d40993122c6e8267556
Ben <91610521+bendaacz@users.noreply.github.com>
2024-04-27 15:45:07 +0000

    feat: added input field and quotes

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react';
 import Loader from 'react-loaders';
 import useLoading from '../../Loading.jsx';
 import "/src/App.scss";
-import Typewriter from "typewriter-effect";
 
 function SignIn() {
     const loading = useLoading();
@@ -17,6 +16,26 @@ function SignIn() {
             return () => clearTimeout(timer);
         }
     }, [loading]);
+
+    const quotes = [
+        "How's your day so far?",
+        "Hello again! We've missed you.",
+        "Forgot something?",
+        "Your return is our delight.",
+        "Back for more? We're thrilled!",
+        "Back in the fold. Let's create magic!",
+    ];
+
+    const [currentQuoteIndex, setCurrentQuoteIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const randomIndex = Math.floor(Math.random() * quotes.length);
+            setCurrentQuoteIndex(randomIndex);
+        }, 100000);
+
+        return () => clearInterval(interval);
+    }, []);
     return (
         <>
             {loading ? (
@@ -24,19 +43,14 @@ function SignIn() {
             ) : (
                 <div className={`h-screen flex ${fadeIn ? 'fade-in' : ''}`}>
                     <div className="w-2/3 h-full bg-[#7cc4ff]">
-                        <div className="bg-[#7cc4ff] text-white flex justify-center mt-[40vh] text-[40px] font-inter">
-                            <Typewriter
-                                options={{
-                                    strings: ["Need more?", "Not enough?", "Have concerns?", "Tight on budget?"],
-                                    autoStart: true,
-                                    pauseFor: 500,
-                                    loop: true,
-                                }}
-                            />
-                        </div>
+                        <h1 className="bg-[#7cc4ff] text-[#333333] flex justify-center mt-[40vh] text-[40px] font-inter">{quotes[currentQuoteIndex]}</h1>
                     </div>
                     <div className="w-1/3 h-full bg-[#bf87ff]">
-                        <p className="flex justify-center mt-[10vh] text-[50px] font-light bg-[#bf87ff]">log in</p>
+                        <p className="flex justify-center mt-[10vh] text-[50px] font-normal bg-[#bf87ff]">log in</p>
+                        <div className="flex flex-row bg-[#bf87ff] items-center content-center justify-center text-center">
+                            <input className="flex text-center placeholder:text-[#484848] mt-[10vh] text-[30px] font-normal bg-[#bf87ff] border" placeholder="username" name="phone" type="username"></input>
+                            <button className="flex text-center" type="submit">submit</button>
+                        </div>
                     </div>
                 </div>
             )}

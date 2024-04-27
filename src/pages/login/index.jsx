607d70d5b028aa52cd02f89035a7b10f27180d13
Ben <91610521+bendaacz@users.noreply.github.com>
2024-04-27 09:32:51 +0000

    feat: login page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import Loader from 'react-loaders';
 import useLoading from '../../Loading.jsx';
 import "/src/App.scss";
+import Typewriter from "typewriter-effect";
 
 function SignIn() {
     const loading = useLoading();
@@ -18,12 +19,25 @@ function SignIn() {
     }, [loading]);
     return (
         <>
-            <Nav />
             {loading ? (
                 <Loader type="ball-grid-pulse" />
             ) : (
-                <div className={`h-[1500px] ${fadeIn ? 'fade-in' : ''}`}>
-                    <p className="font-light text-white text-[24px]">sign in / log in</p>
+                <div className={`h-screen flex ${fadeIn ? 'fade-in' : ''}`}>
+                    <div className="w-2/3 h-full bg-[#7cc4ff]">
+                        <div className="bg-[#7cc4ff] text-white flex justify-center mt-[40vh] text-[40px] font-inter">
+                            <Typewriter
+                                options={{
+                                    strings: ["Need more?", "Not enough?", "Have concerns?", "Tight on budget?"],
+                                    autoStart: true,
+                                    pauseFor: 500,
+                                    loop: true,
+                                }}
+                            />
+                        </div>
+                    </div>
+                    <div className="w-1/3 h-full bg-[#bf87ff]">
+                        <p className="flex justify-center mt-[10vh] text-[50px] font-light bg-[#bf87ff]">log in</p>
+                    </div>
                 </div>
             )}
         </>

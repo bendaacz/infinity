25b77f04a8507372778821496d1cc5314e635d6a
Ben <91610521+bendaacz@users.noreply.github.com>
2024-05-08 09:40:15 +0000

    feat: reworking backend

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const routesHandler = require("./routes/handler")
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use("/", routesHandler);
 
-const PORT = 4000;
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
 app.listen(PORT, () => {
-  console.log("backend běží");
+  console.log(`Server listening on ${PORT}`);
 });
